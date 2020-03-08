4b5ea7b5d948b044896bc982612aaf1d11c8321b
Jacob Steves <jgs1998@hotmail.com>
2020-03-08 11:11:37 -0400

    add custom project commands to help output

--- lib/yeah/commands/help.rb
+++ lib/yeah/commands/help.rb
@@ -4,16 +4,35 @@ module Yeah
   module Commands
     class Help < Yeah::Command
       def call(args, _name)
-        Output.print("{{bold:Available commands}}", newline: true)
+        Output.print("{{bold:Base commands}}")
 
         Yeah::Commands::Registry.resolved_commands.each do |name, klass|
           next if name == 'help'
-          Output.print("{{command:#{Yeah::TOOL_NAME} #{name}}}")
+          Output.newline
+          Output.print(command_usage(name))
           if help = klass.help
             Output.print(help)
           end
-          Output.newline
         end
+
+        project = Yeah::Project.current
+        if project.custom_command_names.any?
+          Output.newline
+          Output.print("{{bold:Project Commands}}")
+
+          project.custom_command_names.each do |name|
+            Output.newline
+            definition = project.config['commands'][name]
+            Output.print(command_usage(name))
+            Output.print(definition['desc']) if definition['desc']
+          end
+        end
+      end
+
+      private
+
+      def command_usage(name)
+        "{{command:#{Yeah::TOOL_NAME} #{name}}}"
       end
     end
   end
